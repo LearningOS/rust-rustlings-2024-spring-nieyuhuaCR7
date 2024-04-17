fe13724ec14af865db3e097c63ea4d24559a396a
Yuhua Nie <yuhuanie@YuhuadeMacBook-Pro.local>
2024-04-16 21:07:23 -0700

    update

--- exercises/conversions/from_str.rs
+++ exercises/conversions/from_str.rs
@@ -10,6 +10,10 @@
 // hint.
 
 use std::num::ParseIntError;
+use crate::ParsePersonError::ParseInt;
+use crate::ParsePersonError::NoName;
+use crate::ParsePersonError::BadLen;
+use crate::ParsePersonError::Empty;
 use std::str::FromStr;
 
 #[derive(Debug, PartialEq)]
@@ -31,7 +35,6 @@ enum ParsePersonError {
     ParseInt(ParseIntError),
 }
 
-// I AM NOT DONE
 
 // Steps:
 // 1. If the length of the provided string is 0, an error should be returned
@@ -52,6 +55,21 @@ enum ParsePersonError {
 impl FromStr for Person {
     type Err = ParsePersonError;
     fn from_str(s: &str) -> Result<Person, Self::Err> {
+        if s.is_empty() {
+            Err(Empty)
+        } else {
+            let p: Vec<&str> = s.split(',').collect();
+            if p.len() != 2 {
+                Err(BadLen)
+            } else if p[0].len() == 0 {
+                Err(NoName)
+            } else {
+                match p[1].parse::<usize>() {
+                    Ok(a) => Ok(Person {name: p[0].to_string(), age: a}),
+                    Err(a) => Err(ParseInt(a)),
+                }
+            }
+        }
     }
 }
 
