7d3d152834d6184f28a0cd53e99e7b0f452371a1
Yuhua Nie <yuhuanie@YuhuadeMacBook-Pro.local>
2024-04-22 14:33:01 -0700

    update

--- exercises/clippy/clippy2.rs
+++ exercises/clippy/clippy2.rs
@@ -3,12 +3,12 @@
 // Execute `rustlings hint clippy2` or use the `hint` watch subcommand for a
 // hint.
 
-
+// I AM NOT DONE
 
 fn main() {
     let mut res = 42;
     let option = Some(12);
-    while let Some(x) = option {
+    if let Some(x) = option {
         res += x;
     }
     println!("{}", res);
